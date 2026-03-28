54874cf353517d34a11c68a063f4a776a7dbc320
Abhay Shukla <abhayshuklaistheone@gmail.com>
2026-03-28 18:30:33 +0530

    Add solution for adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    """
+    Perform one AdaGrad update step.
+    """
+    w = np.array(w)
+    g = np.array(g)
+    G = np.array(G)
+
+    G = G + np.pow(g, 2)
+    w = w - (lr/(np.sqrt(G+eps)))*g
+
+    return w, G
+    
